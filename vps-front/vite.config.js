199ab9e4719e9bb81facc842fb4a98f027bdc7e9
Md Abubakar <abubakar.jawed@gmail.com>
2026-01-09 22:16:56 +0530

    fix frontend in docker

--- vps-front/vite.config.js
+++ vps-front/vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,         // listen on 0.0.0.0 (so other containers can reach it)
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      'frontend',
+      '93.127.199.118.sslip.io',
+      '93.127.199.118',
+      'localhost',
+    ],
+  },
 })
